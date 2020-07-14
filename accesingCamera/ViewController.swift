6fa93f6f1a13f7418d06a1ed58f229ee03db1796
Kate <kate.riordan15@gmail.com>
2020-07-14 11:52:41 -0400

    Initial Commit

---
+++ accesingCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accesingCamera
+//
+//  Created by Kate Riordan on 7/14/20.
+//  Copyright © 2020 Kate Riordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
