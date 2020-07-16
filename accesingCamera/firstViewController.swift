4b29bd2c131581c5ccce43f66d227b209c7d6d37
Kate <kate.riordan15@gmail.com>
2020-07-16 12:19:58 -0400

    Updated button functions

--- accesingCamera/firstViewController.swift
+++ accesingCamera/firstViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class firstViewController: UIViewController {
+class firstViewController:
 
-   var activitiesSelected = ""
+    UIViewController {
+    @IBOutlet weak var act89: UIImageView!
+    @IBOutlet weak var act910: UIImageView!
+    @IBOutlet weak var act1011: UIImageView!
+    @IBOutlet weak var act1112: UIImageView!
+    @IBOutlet weak var act121: UIImageView!
+    @IBOutlet weak var act12: UIImageView!
+    @IBOutlet weak var act23: UIImageView!
+    @IBOutlet weak var act34: UIImageView!
+    @IBOutlet weak var act45: UIImageView!
+    @IBOutlet weak var act56: UIImageView!
+    
+    
+    
+    var activitiesSelected = ""
     var matchAct = [
         "Exercise" : 0,
         "Homework" : 1,
@@ -19,7 +33,7 @@ class firstViewController: UIViewController {
         "Work" : 4
     ]
 
-    var imageHold = [#imageLiteral(resourceName: "man-running-track-round-icon_18591-1469"), #imageLiteral(resourceName: "HOMEWORK-1"), #imageLiteral(resourceName: "shopping-icon-2184065_1280"), #imageLiteral(resourceName: "container-icon-laptop-working-on-laptop-icon-11562980144p8odojgwmj.png"), #imageLiteral(resourceName: "container-icon-laptop-working-on-laptop-icon-11562980144p8odojgwmj")]
+    var imageHold = [#imageLiteral(resourceName: "man-running-track-round-icon_18591-1469"), #imageLiteral(resourceName: "HOMEWORK-1"), #imageLiteral(resourceName: "shopping-icon-2184065_1280"), #imageLiteral(resourceName: "236-2366695_presentacin-en-tu-colegio-o-instituto-circle-school"), #imageLiteral(resourceName: "container-icon-laptop-working-on-laptop-icon-11562980144p8odojgwmj")]
     
     
     override func viewDidLoad() {
@@ -32,16 +46,37 @@ class firstViewController: UIViewController {
     
     
     @IBAction func exerciseButtonPressed(_ sender: UIButton) {
-        activitiesSelected = "exercise"
+        activitiesSelected = "Exercise"
+    }
+    
+    
+    @IBAction func homeworkButtonPressed(_ sender: UIButton) {
+        activitiesSelected = "Homework"
+    }
+    
+    
+    @IBAction func shoppingButtonPressed(_ sender: UIButton) {
+        activitiesSelected = "Shopping"
+    }
+    
+    @IBAction func schoolButtonPressed(_ sender: UIButton) {
+        activitiesSelected = "School"
+    }
+    
+    @IBAction func workButtonPressed(_ sender: UIButton) {
+        activitiesSelected = "Work"
     }
     
     // Schedule buttons
     
     @IBAction func scheduleActivity(_ sender: UIButton) {
         // find which image to use
-        //imageHold = "exercise"
+        let imageIndex = matchAct[activitiesSelected]
+        
         // change image property of button to image of acitivty
-       // func setImage(UIImage?, for: UIControl.State)
+        act89.image = imageHold[imageIndex!]
+        
+        
         // set text to empty string
         // class UITextField : UIControl "" 
     }
